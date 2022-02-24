761ef843ab097396af04a14c8bd37fcf150e8913
Alonso Herreros <alonso.herreros.c@gmail.com>
2022-02-24 23:38:50 +0100

    Add M4 R1-3 base files

---
+++ Mision 04 - Mas alla de la pantalla/Reto 1 - Un sinfin de posibilidades/computePermutations.py
@@ -0,0 +1,11 @@
+
+def factorial(n):
+
+
+
+metaGemas = int(input())
+if metaGemas < 0:
+    print('numero_no_valido')
+    exit()
+permutations = factorial(metaGemas)
+print(permutations)
---
+++ Mision 04 - Mas alla de la pantalla/Reto 2 - En el lugar exacto en el momento preciso/testModel.py
@@ -0,0 +1,87 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import tensorflow.keras as kr
+
+
+# Define global variables
+backgroundImg = plt.imread("granCanariaVistaAerea.jpg")
+
+
+# Load neural network model
+print('Loading artificial neural network model...')
+model = kr.models.load_model('model.h5')
+
+
+# Define function to classify a new sample using the trained model
+def classify(Xnew, model):
+    Ynew = model.predict(Xnew)
+    Ynew = Ynew.flatten()
+    for i in range(len(Ynew)):
+        if Ynew[i] > 0.5:
+            Ynew[i] = 1
+        else:
+            Ynew[i] = 0
+    return Ynew
+
+
+# Load test data
+print('Loading test data...')
+test_positions = []
+
+with open('test_set.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    next(csv_reader, None)  # skip the headers
+    for line in csv_reader:
+        x = float(line[0])
+        y = float(line[1])
+        test_positions.append([x, y])
+test_positions = np.array(test_positions)
+
+
+# Show test data
+fig, ax = plt.subplots(figsize=(12, 6))
+ax.set_title("test data")
+ax.imshow(backgroundImg, extent=[-63, 63, -36, 36])
+ax.scatter(test_positions[:, 0], test_positions[:, 1], s=20, c="skyblue")
+plt.show()
+
+
+# Classify test data using neural network model
+print('Classifying test data...')
+predicted_labels = classify(test_positions, model)
+
+
+# Visualize classified test data by the model
+fig, ax = plt.subplots(figsize=(12, 6))
+ax.set_title("classified test data by the model")
+ax.imshow(backgroundImg, extent=[-63, 63, -36, 36])
+ax.scatter(test_positions[predicted_labels == 0, 0], test_positions[predicted_labels == 0, 1], s=20, color="skyblue")
+ax.scatter(test_positions[predicted_labels == 1, 0], test_positions[predicted_labels == 1, 1], s=20, color="salmon")
+plt.show()
+
+
+# Load classified test data by a human
+print('Loading test data classification by a human...')
+test_set_labels = []
+
+with open('test_set_labels.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    next(csv_reader, None)  # skip the headers
+    for line in csv_reader:
+        label = float(line[0])
+        test_set_labels.append(label)
+test_set_labels = np.array(test_set_labels)
+
+
+# Calculate success rate
+print('Comparing model predictions with human classification...')
+#########################################################################
+
+#########################################################################
+
+
+success_rate = "{:.2f}".format(success_rate)
+print(f'Success rate: {success_rate}%')
